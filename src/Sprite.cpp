c4d3b5bc62002f341c926c0bc62bf77f776c314f
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2012-02-24 19:03:14 +0000

    Minor updates in Sprite

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,9 +1,22 @@
-/*
+/**
  * Sprite.cpp
  *
- *  Created on: 23 févr. 2012
- *      Author: podgy
+ *  @date 23/02/2012
+ *  @author G. B.
  */
+/*        This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA */
 
 #include "Sprite.hpp"
 
@@ -50,5 +63,12 @@ void Sprite::move() {
 bool Sprite::collidesWith(SDL_Surface *obj) {
 	return false;
 }
+
+void AnimatedSprite::draw(SDL_Surface *dest)
+{
+
+}
+
+
 
 
