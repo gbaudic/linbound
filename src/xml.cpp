aaa82febf27460d64de9adf8fbc1f7b9e5ccd053
G. Baudic <g.baudic@isae.fr>
2016-07-26 11:44:24 +0200

    Started functions to manage XML files

---
+++ src/xml.cpp
@@ -0,0 +1,32 @@
+/**
+ * \file xml.cpp
+ * \brief XML-related functions
+ * 		  Part of LinBound game
+ * \author G. B.
+ * \version 0.1a
+ * \date 26/07/2016
+ */
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is “Incompatible With Secondary Licenses”,
+ * as defined by the Mozilla Public License, v. 2.0.
+ */
+
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+using namespace std;
+
+/**
+ * Load a map from the XML description file
+ */
+void LB_LoadMap(const char *filename){
+	XMLDocument doc;
+	doc.LoadFile(filename);
+
+
+}
+
+
